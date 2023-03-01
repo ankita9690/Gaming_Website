7d0bdd4c24b736107b74b8f3231c4680f83ffde0
ankitakundliya <ankita2kundliya@gmail.com>
2023-03-01 19:47:59 +0530

    home.js modified

--- home.js
+++ home.js
@@ -1,6 +1,14 @@
-const express=require("express");
 
+//requiring packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose=require("mongoose");
+//setting packages
 const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
   res.send("hwll");
